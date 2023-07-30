b476c52b7b1355111bf35c31b5af39478939ca7c
IzzetGunturk <izzetgunturk2@hotmail.com>
2023-07-30 16:09:02 +0200

    Seperate the components

---
+++ src/components/ShowCarInformation.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import CarInformation from './CarInformation';
+
+function ShowCarInformation() {
+  const [licensePlateSearch, setLicensePlateSearch] = useState('');
+  const [error, setError] = useState('');
+
+  const handleInputChange = (event) => {
+  setLicensePlateSearch(event.target.value);
+  setError('');
+}
+
+return (
+<section>
+  <div className="mx-auto max-w-screen-xl px-4 py-28 sm:px-6 lg:px-8">
+    <div className="mx-auto">
+      <h1 className="text-center text-2xl font-bold text-primary sm:text-3xl">
+        <span className='text-secondary'>APK</span> expiration date check
+      </h1>
+
+      <p className="mx-auto mt-4 max-w-lg text-center text-gray-500">
+        Check when your APK of your car expires through the form!
+      </p>
+
+      <form className="mt-6 mb-0 space-y-4 rounded-lg p-4 shadow-xl sm:p-6 lg:p-8 mx-auto max-w-xl">
+        <div>
+          <div className="relative">
+            <input type="text" className="w-full rounded-lg p-4 pr-12 text-sm bg-[#ebebeb]"
+              placeholder="Enter your license plate: H405TG" maxLength={6} onChange={handleInputChange} />
+            {licensePlateSearch && <CarInformation licensePlate={licensePlateSearch} setError={setError} /> }
+            <div className='text-red-500 pt-4'>{error}</div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
+
+);
+}
+
+export default ShowCarInformation;
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -9,4 +9,4 @@ function header() {
   )
 }
 
-export default header
+export default header;
